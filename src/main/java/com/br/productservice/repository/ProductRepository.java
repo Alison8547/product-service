797160bef2daec854836b8a52e7f8ce6b2d7988b
Alison8547 <alisonailson100@hotmail.com>
2024-07-07 13:32:52 -0300

    Product model and repository

---
+++ src/main/java/com/br/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.br.productservice.repository;
+
+import com.br.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
