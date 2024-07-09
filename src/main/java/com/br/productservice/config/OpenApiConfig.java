f1d088ff004832e7089b016c2baf5045bd2df2bb
Alison8547 <alisonailson100@hotmail.com>
2024-07-09 11:00:02 -0300

    add url customOpenAPI

--- src/main/java/com/br/productservice/config/OpenApiConfig.java
+++ src/main/java/com/br/productservice/config/OpenApiConfig.java
@@ -16,7 +16,7 @@ public class OpenApiConfig {
                 .info(new Info().title("Product Service")
                         .description("This is a product service")
                         .version("1.0.0")
-                        .license(new License().name("Apache 2.0")))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
                 .externalDocs(new ExternalDocumentation().description("You can refer to the Product Service Wiki Documentation")
                         .url("https://www.apache.org/licenses/LICENSE-2.0.html"));
 
